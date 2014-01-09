245c5f2f6a6fac38a6879b6d7e3789e329b9dbfc
Jeffrey Fiddler <jeffnv@gmail.com>
2014-01-09 11:43:34 -0800

    removed two unneeded lines from feed model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -25,9 +25,6 @@ class Feed < ActiveRecord::Base
     self.touch #this causes the updated_at column to be updated
     begin
       feed_data = SimpleRSS.parse(open(url))
-      self.title = feed_data.title
-      save!
-
       existing_entry_guids = Entry.pluck(:guid).sort
       feed_data.entries.each do |entry_data|
         unless existing_entry_guids.include?(entry_data.guid)
